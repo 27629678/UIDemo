3384321b309cc05aeb15c7d08c4f3c84bd800862
H-YXH <sddz_yuxiaohua@163.com>
2014-05-16 16:14:51 +0800

    add outline for outlinebutton

--- UIDemo/Attributor_ViewController.h
+++ UIDemo/Attributor_ViewController.h
@@ -11,3 +11,41 @@
 @interface Attributor_ViewController : UIViewController
 
 @end
+
+
+/* ******************************************************
+    1/生命周期
+    Instantiated
+    awakeFromNib
+    outlets get set
+    viewDidLoad
+    (when geometry is determined)
+        viewWillLayoutSubviews: and viewDidLayoutSubviews:
+    viewWillAppear: and viewDidAppear:
+        ...
+        viewWillLayoutSubviews: and viewDidLayoutSubviews:
+        ...
+    viewWillDisappear: and viewDidDisappear:
+ 
+    2/广播
+    Notifications
+    - (void)addObserver:(id)observer        // (you) the object to get notified
+               selector:(SEL)methodToInvokeIfSomeingHappens
+                   name:(NSString*)name     // name of radio station (a constant some where)
+                 object:(id)sender          // whoses changes you interest in (nil is anyone's)
+ 
+    - (void)methodToInvokeIfSomeingHappens:(NSNotification*)notification
+    {
+        notification.name       // the name passed above
+        notification.object     // the object sending you the notification
+        notification.userInfo   // infomation about what happened, (ID type)
+    }
+ 
+    Be sure to "Turn Out" when done listening
+    [center removeObserver:self];
+    or
+    [center removeObserver:self name: observerName object:nil];
+ 
+    Failure to remove observer may result in crashers.
+    This is because the NSNotificationCenter keeps on an "unsafe retained" pointer to you.
+ *****************************************************/
--- UIDemo/Attributor_ViewController.m
+++ UIDemo/Attributor_ViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headline;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 @end
 
 @implementation Attributor_ViewController
@@ -58,6 +59,13 @@
     // Do any additional setup after loading the view from its nib.
     
     [self.headline setText:@"UITextView Demo"];
+    
+    NSMutableAttributedString* title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    
+    [title setAttributes:@{NSStrokeWidthAttributeName: @3
+                          , NSStrokeColorAttributeName: self.outlineButton.tintColor}   // tintColor 全局文字颜色
+                   range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
 // 这个方法可能会被调用多次
@@ -66,7 +74,24 @@
 // 可以做更新View中的几何图形更新操作
 - (void)viewWillAppear:(BOOL)animated
 {
+    [super viewWillAppear:animated];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification*)notification
+{
+    [self usePreferredSystemFont];
+}
+
+- (void)usePreferredSystemFont
+{
+    [self.body setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleBody]];
+    
+    [self.headline setFont:[UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -78,6 +103,10 @@
 {
     [super viewWillDisappear:animated];
     
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+    
     // 保存当前场景的状态
 //    [self rememberScrollPosition];
     
@@ -85,10 +114,37 @@
 //    [self saveDataToPermanentStore];
 }
 
+- (void)viewWillLayoutSubviews
+{
+    
+}
+
+- (void)viewDidLayoutSubviews
+{
+    
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    
+}
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    // 内存占用大户，如视频、音频、图像等
 }
 
 @end
